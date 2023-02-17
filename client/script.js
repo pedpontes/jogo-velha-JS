bf85c3c01c2fcd503f3b0118173ff56844e9f22b
Pedro Pontes <pontespeter@gmail.com>
2023-02-17 11:04:06 -0300

    quarto commit

--- client/script.js
+++ client/script.js
@@ -1,28 +1,53 @@
 const square = document.querySelectorAll('.square')
-const tipo = [1,2]
+const button = document.querySelector('#reset')
+const tipo = [{id: 1, color: 'red'},{id: 2, color: 'blue'}]
 const pos = [{x: 0,y:0},{x: 0,y:1},{x: 0,y:2},
             {x: 1,y:0},{x: 1,y:1},{x: 1,y:2},
             {x: 2,y:0},{x: 2,y:1},{x: 2,y:2}]
 var joga = tipo[0]
 
-function Player(x,y){
+function Player(x,y,square){
     this.x = x;
     this.y = y;
+    this.square = square;
+}
+function reset(){
+    fetch('/reset', {method: 'GET'})
+        .then( res => {
+            res.ok ? location.reload() : console.log('Erro ao redefinir a tabela!')
+        })
 }
 
 function enviar(element){
     element = element.target
     id = element.getAttribute('id')
-    let player = new Player(pos[id-1].x, pos[id-1].y)
-    let data = fetch('/send', {
+    let player = new Player(pos[id-1].x, pos[id-1].y, joga.id)
+
+    fetch('/send', {
         method: 'POST',
         headers: {'Content-Type': 'application/JSON'},
         body: JSON.stringify({ player })
-    // console.log(player)
+    })
+    .then(res => res.json())
+    .then( data => {
+        console.log(data)
+        if(data.win){
+            element.style.backgroundColor = joga.color
+            alert(`Jogador ${joga.id} ganhou o GAME`);
+        }
+        else if(data.add){
+            element.style.backgroundColor = joga.color
+            joga.id == 1 
+                ? joga = tipo[1]
+                : joga = tipo[0]
+        }
+        else{
+            alert('Square ja utilizada, escolha outra!')
+        }
     })
 }
 
 square.forEach( element => {
     element.addEventListener('click', enviar)
 })
-
+button.addEventListener('click', reset)
--- index.js
+++ index.js
@@ -8,27 +8,26 @@ const app = express()
 const PORT = 3000
 var tab = [[3],[3],[3]]
 
+function init(player,res){
+    let add = insert(tab,player);
+    if(add){
+        verify(tab)
+            ? res.status(200).json({ add: true, win: true })
+            : res.status(200).json({ add: true, win: false })}
+    else{res.json({ add: false, win: false })}
+}
+
 app.use(express.static(path.join('client')))
 app.use(bodyParser.json())
 
 app.post('/send', (req,res) => {
-    console.log(req.body)
-    // if(!insert(tab,player)){
-    //     if(verify(tab)){
-    //         res.json({ add: false, win: true })
-    //     }
-    //     else{
-    //         res.json({ add: false, win: false })
-    //     }
-    // }
-    // else{
-    //     if(verify(tab)){
-    //         res.json({ add: true, win: true })
-    //     }
-    //     else{
-    //         res.json({ add: true, win: false })
-    //     }
-    // }
+    player = req.body.player
+    init(player, res)
+})
+app.get('/reset', (req,res) => {
+    zerandoTab(tab)
+    res.sendStatus(200)
 })
 
+zerandoTab(tab)
 app.listen(PORT, () => console.log(`Server running in port: ${PORT}`))
